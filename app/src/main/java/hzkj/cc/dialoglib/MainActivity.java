ff9ffa3983a551a21f4e45947d4cca8bc1c5aabe
cc <doublefantansy@outlook.com>
2019-07-15 15:41:24 +0800

    改为dialog

--- app/src/main/java/hzkj/cc/dialoglib/MainActivity.java
+++ app/src/main/java/hzkj/cc/dialoglib/MainActivity.java
@@ -14,25 +14,26 @@ import hzkj.cc.loadingdialog.CcDialog;
  * @author cc
  */
 public class MainActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        XUI.initTheme(this);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ((AttdenceView) findViewById(R.id.s)).setTextSize(20);
-        ((AttdenceView) findViewById(R.id.s)).setText("签到");
-        findViewById(R.id.s).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new CcDialog(MainActivity.this, CcDialog.CHOOSE_DIALOG).setMessage("是否删除该条数据")
-                        .setCancelListener(new CancelListener() {
-                            @Override
-                            public void onClick(CcDialog dialog) {
-                                dialog.dismissDialog();
-                            }
-                        })
-                        .showDialog();
-            }
-        });
-    }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    XUI.initTheme(this);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    ((AttdenceView) findViewById(R.id.s)).setTextSize(20);
+    ((AttdenceView) findViewById(R.id.s)).setText("签到");
+    findViewById(R.id.s).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        new CcDialog(MainActivity.this, CcDialog.SUCCESS_DIALOG).setMessage("是否删除该条数据")
+            .setCancelListener(new CancelListener() {
+              @Override
+              public void onClick(CcDialog dialog) {
+                dialog.dismissDialog();
+              }
+            })
+            .showDialog();
+      }
+    });
+  }
 }
--- loadingdialog/src/main/java/hzkj/cc/loadingdialog/CcDialog.java
+++ loadingdialog/src/main/java/hzkj/cc/loadingdialog/CcDialog.java
@@ -12,104 +12,108 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class CcDialog extends View {
-    public static final int PROGRESS_DIALOG = 0;
-    public static final int SUCCESS_DIALOG = 1;
-    public static final int FAIL_DIALOG = 2;
-    public static final int CHOOSE_DIALOG = 32;
-    private int type;
-    Dialog dialog;
-    private HookView hookView;
-    private TextView msg;
-    private Button button;
-    private CancelListener cancelListener;
-    private ErrorView errorView;
 
-    public CcDialog(Context context, int type) {
-        super(context);
-        this.type = type;
-        init();
-    }
+  public static final int PROGRESS_DIALOG = 0;
+  public static final int SUCCESS_DIALOG = 1;
+  public static final int FAIL_DIALOG = 2;
+  public static final int CHOOSE_DIALOG = 32;
+  private int type;
+  Dialog dialog;
+  private HookView hookView;
+  private TextView msg;
+  private Button button;
+  private CancelListener cancelListener;
+  private ErrorView errorView;
 
-    private void init() {
-        dialog = new Dialog(getContext());
-        switch (type) {
-            case PROGRESS_DIALOG: {
-                View view = LayoutInflater.from(getContext())
-                        .inflate(R.layout.loading, null, false);
-                dialog.setContentView(view);
-                ImageView image = view.findViewById(R.id.image);
-                msg = view.findViewById(R.id.tv_text);
-                ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(image, "rotation", 0, 360);
-                objectAnimator.setDuration(3000);
-                LinearInterpolator linearInterpolator = new LinearInterpolator();
-                objectAnimator.setInterpolator(linearInterpolator);
-                objectAnimator.setRepeatCount(ValueAnimator.INFINITE);
-                objectAnimator.start();
-                break;
-            }
-            case SUCCESS_DIALOG: {
-                View view = LayoutInflater.from(getContext())
-                        .inflate(R.layout.success, null, false);
-                dialog.setContentView(view);
-                hookView = view.findViewById(R.id.hookView);
-                hookView.startCircle();
-                msg = view.findViewById(R.id.text);
-                button = view.findViewById(R.id.cancel);
-                break;
-            }
-            case FAIL_DIALOG: {
-                View view = LayoutInflater.from(getContext())
-                        .inflate(R.layout.fail, null, false);
-                dialog.setContentView(view);
-                errorView = view.findViewById(R.id.errorView);
-                errorView.startCircle();
-                msg = view.findViewById(R.id.text);
-                button = view.findViewById(R.id.cancel);
-                break;
-            }
-            case CHOOSE_DIALOG: {
-                View view = LayoutInflater.from(getContext())
-                        .inflate(R.layout.choose, null, false);
-                dialog.setContentView(view);
+  public CcDialog(Context context, int type) {
+    super(context);
+    this.type = type;
+    init();
+  }
+
+  private void init() {
+    dialog = new Dialog(getContext());
+    switch (type) {
+      case PROGRESS_DIALOG: {
+        dialog.setCanceledOnTouchOutside(false);
+        View view = LayoutInflater.from(getContext())
+            .inflate(R.layout.loading, null, false);
+        dialog.setContentView(view);
+        ImageView image = view.findViewById(R.id.image);
+        msg = view.findViewById(R.id.tv_text);
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(image, "rotation", 0, 360);
+        objectAnimator.setDuration(3000);
+        LinearInterpolator linearInterpolator = new LinearInterpolator();
+        objectAnimator.setInterpolator(linearInterpolator);
+        objectAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        objectAnimator.start();
+        break;
+      }
+      case SUCCESS_DIALOG: {
+        View view = LayoutInflater.from(getContext())
+            .inflate(R.layout.success, null, false);
+        dialog.setContentView(view);
+        hookView = view.findViewById(R.id.hookView);
+        hookView.startCircle();
+        msg = view.findViewById(R.id.text);
+        button = view.findViewById(R.id.cancel);
+        dialog.setCanceledOnTouchOutside(false);
+        break;
+      }
+      case FAIL_DIALOG: {
+        View view = LayoutInflater.from(getContext())
+            .inflate(R.layout.fail, null, false);
+        dialog.setContentView(view);
+        errorView = view.findViewById(R.id.errorView);
+        errorView.startCircle();
+        msg = view.findViewById(R.id.text);
+        button = view.findViewById(R.id.cancel);
+        dialog.setCanceledOnTouchOutside(false);
+        break;
+      }
+      case CHOOSE_DIALOG: {
+        View view = LayoutInflater.from(getContext())
+            .inflate(R.layout.choose, null, false);
+        dialog.setContentView(view);
 //                errorView = view.findViewById(R.id.errorView);
 //                errorView.startCircle();
-                msg = view.findViewById(R.id.text);
-                button = view.findViewById(R.id.cancel);
-                break;
-            }
+        dialog.setCanceledOnTouchOutside(true);
+        msg = view.findViewById(R.id.text);
+        button = view.findViewById(R.id.cancel);
+        break;
+      }
+    }
+    dialog.setCancelable(true);
+    dialog.getWindow()
+        .setBackgroundDrawableResource(android.R.color.transparent);
+  }
+
+  public void showDialog() {
+    dialog.show();
+  }
+
+  public void dismissDialog() {
+    dialog.dismiss();
+  }
+
+  public CcDialog setCancelListener(final CancelListener cancelListener) {
+    this.cancelListener = cancelListener;
+    if (button != null) {
+      button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          dialog.dismiss();
+          if (CcDialog.this.cancelListener != null) {
+            CcDialog.this.cancelListener.onClick(CcDialog.this);
+          }
         }
-        dialog.setCanceledOnTouchOutside(false);
-        dialog.setCancelable(false);
-        dialog.getWindow()
-                .setBackgroundDrawableResource(android.R.color.transparent);
+      });
     }
+    return this;
+  }
 
-    public void showDialog() {
-        dialog.show();
-    }
-
-    public void dismissDialog() {
-        dialog.dismiss();
-    }
-
-    public CcDialog setCancelListener(final CancelListener cancelListener) {
-        this.cancelListener = cancelListener;
-        if (button != null) {
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    dialog.dismiss();
-                    if (CcDialog.this.cancelListener != null) {
-                        CcDialog.this.cancelListener.onClick(CcDialog.this);
-                    }
-                }
-            });
-        }
-        return this;
-    }
-
-    public CcDialog setMessage(String message) {
-        msg.setText(message);
-        return this;
-    }
+  public CcDialog setMessage(String message) {
+    msg.setText(message);
+    return this;
+  }
 }
